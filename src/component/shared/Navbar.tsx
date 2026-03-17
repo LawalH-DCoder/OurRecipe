a9e6826299eb9f1e2b007f271a4dc09846fc33f0
Jubril Oshisanwo <Jubril.Oshisanwo@interswitchng.com>
2026-03-17 09:34:54 +0100

    chore: fixed navbar

--- src/component/shared/Navbar.tsx
+++ src/component/shared/Navbar.tsx
@@ -1,11 +1,21 @@
 import { Link } from "react-router-dom";
+import "./navbar.css";
 
 function Navbar() {
   return (
-    <nav style={{ display: "flex", gap: "20px", padding: "20px" }}>
-      <Link to="/">Home</Link>
-      <Link to="/recipes">Recipes</Link>
-      <Link to="/add-recipe">Add Recipe</Link>
+    <nav className="navbar ">
+     <h2 className="text-2xl font-bold text-white">
+              Recip
+              <em className="not-italic italic text-[#F97316]">io</em>
+            </h2>
+      <div className="navbar-links">
+        <Link to="/" className="navbar-link"><p>Home</p></Link>
+        <Link to="/recipes" className="navbar-link"><p>Explore</p></Link>
+        <Link to="/add-recipe" className="navbar-link"><p>Favourites</p></Link>
+        <button className="navbar-button">
+          Random Picks
+        </button>
+      </div>
     </nav>
   );
 }
